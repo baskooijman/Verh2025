91c899935dd0e3c169cf58ea2661d8ec8b0db98a
Bas Kooijman <bas.kooijman@vu.nl>
2025-11-10 08:22:00 +0100

    VerhKooy2025_SI

--- VerhKooy2025_SI.m
+++ VerhKooy2025_SI.m
@@ -1046,6 +1046,88 @@ for c=1:length(fig)
       ylabel('_{10}log spec O_2 consumption J_O^\infty/ W_w^\infty, mol/d.g')
       saveas(gca,'ss_jOi.png')
 
+    case 12 % care-corrected s_s
+      shstat_options('default');
+      shstat_options('x_transform', 'none');
+      shstat_options('y_transform', 'none');
+      shstat_options('x_label', 'on');
+      shstat_options('y_label', 'on');
+      data = read_allStat({'p_Ji','p_Si','p_Ai','kap'}); 
+      p_Ji = data(:,1); p_Si = data(:,2); p_Ai = data(:,3); kap = data(:,4);
+      s_s = p_Ji.*p_Si.^2./p_Ai.^3; % -, supply stress
+      
+      [Hfig, Hleg] = shstat([s_s, kap], legend_aves, date); % set title, output handle for adding items    
+      figure(Hfig) % care-corrected  s_s-kap
+      xlabel('supply stress, s_s, -')
+      ylabel('allocation fraction to soma, \kappa, -')
+      text(0.01,0.5,'\kappa_J^M = 0')
+      kkap = linspace(0, 1, 100); ss = kkap.^2 .* (1 - kkap); 
+      plot(ss, kkap, 'k', 'Linewidth', 2)
+      xlim([0 4/27]); ylim([0 1]);
+      saveas(gca,'ss_kap.png')
+      %
+      shlegend(legend_aves, [0.7 0.2], [0.8 0.2]);
+      saveas(gca,'legend_aves.png')
+      
+      % care-corrected s_s
+      kap_JM = 0.025;
+      s_scor = (p_Ji + kap_JM * p_Si)*(1-kap_JM)^2.*p_Si.^2./p_Ai.^3; % -, care-corrected supply stress
+      [Hfig, Hleg] = shstat([s_scor, (1 - kap_JM)*kap], legend_aves, date); % set title, output handle for adding items
+    
+      figure(Hfig) % care-corrected s_s-care-corrected kap
+      xlabel('care-corrected supply stress, s_s^c, -')
+      ylabel('corr alloc frac to soma, (1 - \kappa_J^M) \kappa, -')
+      text(0.01,0.5,['\kappa_J^M = ', num2str(kap_JM)])
+      kkap = linspace(0, 1, 100)'; ss = kkap.^2 .* (1 - kkap); 
+      plot(ss, kkap, 'k', 'Linewidth', 2)
+      xlim([0 4/27]); ylim([0 1]);
+      saveas(gca,'ssc_kap.png')
+
+      nm = select; nm_ave = ave(:,4); n_ave = length(nm_ave); FMR_ave = NaN(n_ave,1);
+      data_ave = cell2mat(ave(:,1)); PMR_ave = data_ave(:,4); T_ave = data_ave(:,2); W_ave = data_ave(:,1); 
+      for i=1:n_ave
+        FMR_ave(i) = 15.55 * get_FMR(nm_ave{i}, W_ave(i), T_ave(i), 1); % ml O2/min
+      end
+      FAS_ave = PMR_ave./FMR_ave;  data = read_stat(nm_ave,{'p_Ji','p_Si','p_Ai','kap'}); 
+      p_Ji = data(:,1); p_Si = data(:,2); p_Ai = data(:,3); kap = data(:,4);
+      s_s_ave = p_Ji.*p_Si.^2./p_Ai.^3; % -, supply stress
+
+      Hfig = figure; % ss-FAS for birds
+      plot([0;4/27],[1/2;3/2],'-', 'Color',[.85 .85 .85], 'LineWidth',15) % grey "model" prediction
+      hold on
+      plot(s_s_ave, log10(FAS_ave), '.r', 'MarkerSize',20)
+
+      xlabel('supply stress s_s, -')
+      xlim([0 4/27])
+      ylabel('_{10}log measured PMR/ predicted FMR, -')
+       
+      % set species names behind markers in plot figure
+      h = datacursormode(Hfig); entries_txt = nm_ave; data = [s_s_ave, log10(FAS_ave)];
+      h.UpdateFcn = @(obj, event_obj)xylabels(obj, event_obj, entries_txt, data);
+      datacursormode on % mouse click on plot
+      text(.02, 2, '\kappa_J^M = 0')
+
+      saveas(gcf,'ss_FAS_ave.fig')
+      saveas(gcf,'ss_FAS_ave.png')
+
+      Hfig = figure; % care-corrected ss-FAS for birds
+      plot([0;4/27],[1/2;3/2],'-', 'Color',[.85 .85 .85], 'LineWidth',15) % grey "model" prediction
+      hold on
+      s_scor_ave = (p_Ji + kap_JM * p_Si)*(1-kap_JM)^2.*p_Si.^2./p_Ai.^3; % -, care-corrected supply stress
+      plot(s_scor_ave, log10(FAS_ave), '.r', 'MarkerSize',20)
+      xlabel('care corr supply stress s_s^c, -')
+      xlim([0 4/27])
+      ylabel('_{10}log measured PMR/ predicted FMR, -')
+      text(.02, 2, ['\kappa_J^M = ', num2str(kap_JM)])
+       
+      % set species names behind markers in plot figure
+      h = datacursormode(Hfig); entries_txt = nm_ave; data = [s_scor_ave, log10(FAS_ave)];
+      h.UpdateFcn = @(obj, event_obj)xylabels(obj, event_obj, entries_txt, data);
+      datacursormode on % mouse click on plot
+
+      saveas(gcf,'ssc_FAS_ave.fig')
+      saveas(gcf,'ssc_FAS_ave.png')
+
   end
 end
    
@@ -1563,7 +1645,8 @@ end
 %   year = {1981},
 %   pages = {197-204}
 % }
-%% @ARTICLE{WortWort2013,
+%
+% @ARTICLE{WortWort2013,
 %   doi = {10.1111/mms.12091}, 
 %   author = {Graham A. J. Worthy and Tamara A. M. Worthy and Pamela K. Yochem},
 %   title = {Basal metabolism of an adult male killer whale (Orcinus orca)},
